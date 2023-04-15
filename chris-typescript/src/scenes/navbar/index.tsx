a83e029f7fbdd396a2fc7a5813fe3726a91007bc
Chriscodinglife <corella4@pratt.edu>
2023-04-14 20:54:54 -0400

    pushing thru tutorial

--- chris-typescript/src/scenes/navbar/index.tsx
+++ chris-typescript/src/scenes/navbar/index.tsx
@@ -7,19 +7,23 @@ import useMediaQuery from "@/hooks/useMediaQuery";
 import ActionButton from "@/shared/ActionButton";
 
 type Props = {
+  isTopOfPage: boolean;
   selectedPage: SelectedPage;
   setSelectedPage: (value: SelectedPage) => void;
 };
 
-const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
+const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
   const flexBetween = "flex items-center justify-between"; // common ui classes to center items and justify between
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)"); // this is a custom hook that returns true if the screen is above 1060px
   const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false); // this is used to toggle the menu when the screen is below 1060px
+  const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow";
 
   return (
     <nav>
       {/* top nav full bar */}
-      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+      <div
+        className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}
+      >
         {/* inner nav bar, basically the island inside the top bar */}
         <div className={`${flexBetween} mx-auto w-5/6`}>
           {/* this will be used to set a gap between objects within the inner nav bar */}
@@ -55,6 +59,7 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
                 </div>
                 <div className={`${flexBetween} gap-8`}>
                   <p>Sign In</p>
+                  {/* place an action button here */}
                   <ActionButton setSelectedPage={setSelectedPage}>
                     Become a Member
                   </ActionButton>
@@ -85,7 +90,7 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
             </button>
           </div>
           {/* Menu Items */}
-          <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+          <div className="ml-[25%] flex flex-col gap-10 text-2xl">
             <Link
               page="Home"
               selectedPage={selectedPage}
